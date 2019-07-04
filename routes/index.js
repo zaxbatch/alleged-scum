3f0c7e80de6b76f97990f9eb3124b0a78549bbbc
Dotcom <sneakjunkmail@gmail.com>
2019-07-03 23:41:34 -0400

    worked on add page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Alleged Scum' });
 });
 
+router.get('/add', function(req, res, next) {
+  res.render('add', { title: 'Add Scumbag' });
+});
+
 module.exports = router;
