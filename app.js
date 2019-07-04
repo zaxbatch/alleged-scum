16d577c37cce81a9715edf4c4d1c152e801b79dd
Dotcom <sneakjunkmail@gmail.com>
2019-07-04 01:06:02 -0400

    mongodb connected

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +21,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//mongo
+mongoose.connect("mongodb://localhost:27017/alleged-scumbag", { useNewUrlParser: true } );
+var db = mongoose.connection;
+// mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('mongodb connected successfully')
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
